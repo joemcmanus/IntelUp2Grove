4541ac572f45c66012035eb5bb00f4ee38b1b2fb
Joe McManus <joe.mcmanus@colorado.edu>
2018-11-05 21:52:17 -0700

    First example added

---
+++ led.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# File    : led.py A sample script for the Intel UP^2 board with GrovePi+ shield
+# Author  : Joe McManus josephmc@alumni.cmu.edu
+# Version : 0.1  11/05/2018 Joe McManus
+# Copyright (C) 2018 Joe McManus
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#This example assumes you have the Grove LED Socket on D8 
+
+import mraa
+import time
+
+#The UP^2 treats the shield as a sub platform over I2C 
+mraa.addSubplatform(mraa.GROVEPI, "0")
+
+#As a sub platform pins start numbering at 512, so D8 is 520
+pin=mraa.Gpio(520)
+
+#Set the direction to out (i.e. write)
+pin.dir(mraa.DIR_OUT)
+
+#Turn it off before we start
+pin.write(0)
+while True:
+	pin.write(1)
+	time.sleep(1)
+	pin.write(0)
+	time.sleep(1)
